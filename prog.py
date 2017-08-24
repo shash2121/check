3b5453136e2b7463cbe535fa74cbf042ce192d7e
shash2121 <31090337+shash2121@users.noreply.github.com>
2017-08-24 11:06:45 +0530

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,11 @@
+x=(input("Enter the character"))
+if(x.isalpha()):
+    print("it's an alphabet")
+elif(x.isdigit()):
+    print("it's a digit")
+elif(x.isspace()):
+    print("it's a space")
+else:
+    print("It's a special symbol")
+    
+    
